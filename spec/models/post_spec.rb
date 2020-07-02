f40bf4f544a29c10a8fb2aef9f37289553df6658
Christopher Amanor <kriskit7@gmail.com>
2020-07-02 13:33:06 +0000

    Add RSpec tests to Post model

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:content) }
+    it { should validate_length_of(:content).is_at_most(1000)}
+  end
+
+  describe 'associations' do
+    it{ should have_many(:comments) }
+    it{ should have_many(:likes) }
+    it{ should belong_to(:user) }
+  end
+
+end
