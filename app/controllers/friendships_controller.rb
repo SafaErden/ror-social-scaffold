9224ce059f5164f93ae9ed4218e8547031b28318
Christopher Amanor <kriskit7@gmail.com>
2020-07-03 13:45:49 +0000

    Add confirm feature to milestone 5

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,10 +1,27 @@
 class FriendshipsController < ApplicationController
   before_action :authenticate_user!
 
-  def index
+  def friendship_request
     @user_to_be_friend = User.find_by(id: params[:user])
     @friendship = Friendship.new(user_id: current_user.id, friend_id: @user_to_be_friend.id, confirmed: false)
-    @friendship.save
-    redirect_to users_path, notice: 'You successfully sent a friendship request.'
+
+    if @friendship.save
+      redirect_to users_path, notice: 'You successfully sent a friendship request.'
+    else
+      redirect_to users_path, notice: 'Something went wrong.'
+    end
   end
+
+  def confirm
+    @user = User.find_by(id: params[:user])
+
+    if current_user.confirm_friend(@user)
+      redirect_to users_path, notice: "You are now friends with #{@user.name}."
+    else
+      redirect_to users_path, notice: 'Something went wrong.'
+    end
+
+  end
+
+
 end
