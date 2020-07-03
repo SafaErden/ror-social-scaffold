44dadf2aded4b3f803c51e0a2c84cccb6c0b2619
SafaErden <safa@DESKTOP-F1CKM19.localdomain>
2020-07-03 12:30:48 +0300

    add request friendship controller and acitons

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,11 @@
+class FriendshipsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @user_to_be_friend = User.find_by(id: params[:user])
+    @friendship=Friendship.new(user_id: current_user.id, friend_id: @user_to_be_friend.id, confirmed: false)
+    @friendship.save
+    redirect_to users_path, notice: 'You successfully sent a friendship request.'
+  end
+  
+end
