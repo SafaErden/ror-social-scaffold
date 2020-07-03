a44b1803661e5b14bcafe32117dae404f5b05500
SafaErden <safa@DESKTOP-F1CKM19.localdomain>
2020-07-03 12:34:38 +0300

    fix rubocop errors

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,9 +3,8 @@ class FriendshipsController < ApplicationController
 
   def index
     @user_to_be_friend = User.find_by(id: params[:user])
-    @friendship=Friendship.new(user_id: current_user.id, friend_id: @user_to_be_friend.id, confirmed: false)
+    @friendship = Friendship.new(user_id: current_user.id, friend_id: @user_to_be_friend.id, confirmed: false)
     @friendship.save
     redirect_to users_path, notice: 'You successfully sent a friendship request.'
   end
-  
 end
