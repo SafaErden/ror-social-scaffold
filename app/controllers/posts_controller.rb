058f392f18411fd79f08aee9ca6ce747a4af7a57
Christopher Amanor <kriskit7@gmail.com>
2020-07-07 08:41:19 +0000

    Refactor viewing friends only posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,9 +20,9 @@ class PostsController < ApplicationController
   private
 
   def timeline_posts
-    # friend_ids = current_user.friends.pluck(:id)
-    # friends_posts = Post.where(user_id: friend_ids).includes(:user)
-    @timeline_posts ||= Post.all.ordered_by_most_recent.includes(:user) #+ friends_posts
+    friend_ids = current_user.friends.pluck(:id)
+    friends_posts = Post.where(user_id: friend_ids).includes(:user)
+    @timeline_posts ||= (current_user.posts.includes(:user) + friends_posts).sort_by(&:created_at).reverse
   end
 
   def post_params
